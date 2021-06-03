5e74021c48d84c885eafcf6018f4c9c9d84b3e57
Dev4LifeV <vctudo.27@gmail.com>
2021-06-03 17:10:34 -0300

    adicionado produtos-api

---
+++ produtos-api/src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Produto } from './produto.model';
+import { ProdutosController } from './produtos.controller';
+import { ProdutosService } from './produtos.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql', // dialeto do banco de dados
+      host: 'localhost', // endereço do servidor do banco
+      port: 3306, // porta do banco de dados
+      username: process.env.USUARIO_BANCO_DADOS, // usuário do MySQL
+      password: process.env.SENHA_BANCO_DADOS, // senha do usuário MySQL
+      database: 'produtos', // nome do banco de dados
+      autoLoadModels: true, // flag para definir se os modelos serão carregados automaticamente
+      synchronize: true // flag para definir se os modelos serão sincronizados automaticamente
+    }),
+
+    /** Método @forFeature que definirá quais models que serão registrados no escopo atual*/
+    /** Podemos injetar o @Produto dentro de ProdutosService*/ 
+    SequelizeModule.forFeature([Produto])
+  ],
+  controllers: [AppController, ProdutosController],
+  providers: [AppService, ProdutosService],
+})
+export class AppModule {}
